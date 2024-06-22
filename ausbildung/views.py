d5b04364c08f883b71cb6bfdabf0d9fd7ed4dfb3
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-06-22 14:03:42 +0200

    views erstellt

--- ausbildung/views.py
+++ ausbildung/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
+
+def gruppe(request):
+    anzahl = Gruppe.objects.all().count()
+    return HttpResponse(f"<h1>{anzahl} Gruppen angelegt</h1>")
